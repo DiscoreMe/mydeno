ca06b66cf38128d42ef574dbe415bb598ec8697e
Nikita <nikitabikmaev@gmail.com>
2021-01-27 20:54:02 +0300

    project: init

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use rusty_v8 as v8;
+
+fn main() {
+    let platform = v8::new_default_platform().unwrap();
+    v8::V8::initialize_platform(platform);
+    v8::V8::initialize();
+
+    let isolate = &mut v8::Isolate::new(v8::CreateParams::default());
+    let handle_scope =  &mut v8::HandleScope::new(isolate);
+    let context = v8::Context::new(handle_scope);
+    let scope = &mut v8::ContextScope::new(handle_scope, context);
+    let code = v8::String::new(scope, "'Hello' + ' world!'").unwrap();
+    let script = v8::Script::compile(scope, code, None).unwrap();
+    let result = script.run(scope).unwrap();
+    let result = result.to_string(scope).unwrap();
+    println!("{}", result.to_rust_string_lossy(scope));
+
+    unsafe {
+        v8::V8::dispose();
+    }
+    v8::V8::shutdown_platform();
+}
